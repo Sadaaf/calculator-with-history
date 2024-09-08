3354b54a2a1363e2fa913feaacc2042ec00f3791
Sadaf Al Arabi <35129264+Sadaaf@users.noreply.github.com>
2024-09-08 15:27:52 -0400

    Added a show hide button for restore functionality

--- src/components/history/HistoryItem.jsx
+++ src/components/history/HistoryItem.jsx
@@ -1,16 +1,27 @@
 import PropTypes from "prop-types";
 import Button from "../ui/Button";
+import { useState } from "react";
 
 const HistoryItem = ({ history, restoreHistory, disabled }) => {
+  const [show, setShow] = useState(false);
+
+  const handleToggle = ()=>{
+    setShow(!show)
+  }
+
   return (
     <li key={history.id}>
       {history.inputs.a} {history.operation} {history.inputs.b} ={" "}
-      {history.result} <small>{history.time}</small>{" "}
-      <Button
-        onClick={() => restoreHistory(history)}
-        disabled={disabled}
-        text="Restore"
-      />
+      {history.result}
+      <Button text={show ? "Hide" : "Show"} onClick={handleToggle} />
+      {show && <>
+        <small>{history.time}</small>{" "}
+        <Button
+          onClick={() => restoreHistory(history)}
+          disabled={disabled}
+          text="Restore"
+        />
+      </>}
     </li>
   );
 };
